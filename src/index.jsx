e0fae0d19ce6a36eafc5ec60c4a4ecba2e91479a
snaxdd <nwpress@yandex.ru>
2022-10-23 13:18:56 +0300

    added firebase

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { initializeApp } from "firebase/app";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.scss";
 import App from "./app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD3tQ_VLHxyElajpN9TnBq4Uw8LTKd0oWE",
+  authDomain: "mapbox-practice.firebaseapp.com",
+  projectId: "mapbox-practice",
+  storageBucket: "mapbox-practice.appspot.com",
+  messagingSenderId: "807675173855",
+  appId: "1:807675173855:web:b5b8645c8781cce323c26a",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
 
 const app = document.getElementById("root");
 const root = ReactDOM.createRoot(app);
